be47b53cc73d00997f74553b44f5059a4ac5d2a5
王涛 <wangtao@licaigc.com>
2016-06-08 00:32:12 +0800

    ad "remove" function to bitmap

--- file/bitmap.py
+++ file/bitmap.py
@@ -11,9 +11,8 @@ class BitMap(object):
     def __init__(self, max_val):
         self._bitmap = [0] * (max_val / 32 + 1)
 
-    def mark(self, val):
-        index = val / 32
-        bit = val % 32
+    def set(self, val):
+        index, bit = self._get_index_and_bit(val)
         self._bitmap[index] |= 2 ** bit
 
     def get(self):
@@ -21,3 +20,10 @@ class BitMap(object):
             for i in range(32):
                 if 2 ** i & item:
                     yield index * 32 + i
+
+    def remove(self, val):
+        index, bit = self._get_index_and_bit(val)
+        self._bitmap[index] &= ((2 ** bit) ^ 0xFFFFFFFF)
+
+    def _get_index_and_bit(self, val):
+        return val / 32, val % 32
